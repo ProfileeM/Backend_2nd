c71992d0f5a83ecdc101c649e955a0c528d84489
student079 <wjdwlstjd968@hanyang.ac.kr>
2024-02-13 21:43:54 +0900

    feature: swagger 추가

--- src/main/java/com/profileeM/profileeM/group/controller/GroupController.java
+++ src/main/java/com/profileeM/profileeM/group/controller/GroupController.java
@@ -4,6 +4,9 @@ import com.profileeM.profileeM.ApiResponse;
 import com.profileeM.profileeM.group.domain.Group;
 import com.profileeM.profileeM.group.service.GroupService;
 import com.profileeM.profileeM.jwt.JwtAuthenticationProvider;
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.media.Schema;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -15,13 +18,15 @@ import org.springframework.web.bind.annotation.RestController;
 @RequestMapping("/group")
 @RequiredArgsConstructor
 @Slf4j
-public class GroupController extends ApiResponse<Group> {
+@Tag(name = "Group Controller", description = "그룹 관련 컨트롤러")
+public class GroupController {
 
     private final GroupService groupService;
     private final JwtAuthenticationProvider jwtAuthenticationProvider;
 
     @PostMapping("/")
-    public ApiResponse<?> createGroup(@RequestBody Group group) {
+    @Operation(summary = "그룹 생성", description = "요청한 사용자가 그룹장이 되어 그룹을 생성합니다.")
+    public ApiResponse<?> createGroup(@RequestBody @Schema(example = "{ \"groupName\": \"My Group\", \"theme\": \"My Theme\" }")Group group) {
         log.debug(group.toString());
         return ApiResponse.ok(groupService.createGroup(group, jwtAuthenticationProvider.getUserId()));
     }

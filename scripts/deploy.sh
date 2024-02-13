6962cd15c4d849561b4816b428c0d91f3e3493cf
student079 <wjdwlstjd968@hanyang.ac.kr>
2024-02-13 16:01:13 +0900

    fix: deploy 수정

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -12,7 +12,7 @@ if [ -z "$CURRENT_PID" ]; then
   echo "현재 구동 중인 애플리케이션이 없으므로 종료하지 않습니다."
 else
   echo "> kill -9 $CURRENT_PID"
-  kill -9 $CURRENT_PID
+  fuser -k 8080/tcp
   sleep 5
 fi
 
@@ -27,7 +27,5 @@ echo "> $JAR_NAME 에 실행권한 추가"
 chmod +x $JAR_NAME
 
 echo "> $JAR_NAME 실행"
-
-source ~/.bashrc
 
 nohup java -jar -Duser.timezone=Asia/Seoul -Djasypt.encryptor.password=$JASYPT_ENCRYPTOR_PASSWORD $JAR_NAME >> $REPOSITORY/nohup.out 2>&1 &
